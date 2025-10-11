9f08786430c80b3921c3fd5399453caaacc96ca8
annekekleppe <anneke@warmerkleppe.nl>
2025-10-11 16:55:37 +0200

    Adjusted content for showing alternative namespaces and private properties

--- src/externals.ts
+++ src/externals.ts
@@ -1,4 +1,5 @@
-// import { setCustomComponents } from "@freon4dsl/core-svelte"
+import { setCustomComponents } from "@freon4dsl/core-svelte"
+import ExternalAccordion from "./lib/accordion/ExternalAccordion.svelte";
 
 
 /**
@@ -8,4 +9,11 @@ export function configureExternals() {
     /**
      * Make the external components known to Freon before starting the app!
      */
+    setCustomComponents([
+        // {component: Button, knownAs: "EButton"},
+        {component: ExternalAccordion, knownAs: "ExternalAccordion"},
+        // {component: ECollapsible, knownAs: "ECollapsible"},
+        // {component: DatePicker, knownAs: "EDatePicker"},
+        // {component: TabComponent, knownAs: "ETab"},
+    ]);
 }
